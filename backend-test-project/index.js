a37c23ace3f4d88952cbdd8b988068bd06ee5102
Jerson2001 <yerson200129@gmail.com>
2020-09-21 20:58:59 -0500

    frontend

--- backend-test-project/index.js
+++ backend-test-project/index.js
@@ -18,7 +18,6 @@ app.get("/descripcion", (req, res) => {
   res.send(`<h1>TenderNote</h1>
             <p>Aplicativo móvil para los tenderos independientes sobre el manejo y el control del inventario que estos llevan a sus locales. La aplicación cuenta con un registro de usuario (tendero) donde posteriormente esta información será guardada en una base de datos, al ingresar el usuario al aplicativo, podrá visualizar sus productos en una imagen con sus respectivos precios y cantidades y dará la opción de controlar su inventario sea porque se reciben productos de parte de su proveedor o se venden a sus clientes.</p>`);
 });
-
 app.listen(5057, function () {
   console.log("El servidor está activo en el puerto 5057");
 });
