0976b136e9c6cbe93b2c6f4f458dd3ed7156f0fc
S-G-L <elsebasgallego2528@gmail.com>
2020-10-02 06:57:02 -0500

    Pablo, before you cry, learn to use git

--- frontend-test-project/src/components/App.js
+++ frontend-test-project/src/components/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
-import logo from '../images/logo.png'
+import logoTenderNote from '../images/TenderNote.jpg'
 import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div class="h-100">
-        <div class="mt-5 mx-auto d-flex justify-content-center">
-          <img src={logo} alt="" srcset="" />
+    <div class="h-100 my-4">
+      <div class="col mt-4">
+        <div class="col-md-6 mx-auto d-flex justify-content-center mt-4">
+          <img src={logoTenderNote} alt="" srcset="" />
+        </div>
       </div>
-      <div class="col mt-5">
-        <div class="mx-auto d-flex justify-content-center flex-wrap align-middle">
-          <button class="btn btn-azul m-2 btn-lg btn-block rounded">
+      <div class="col">
+        <div class="col-md-10 mx-auto d-flex justify-content-center flex-wrap align-middle">
+          <button class="btn btn-azul m-2 btn-lg btn-block rounded-pill">
             <Link to="/login" class="text-blanco">Iniciar sesión</Link>
           </button>
-          <button class="mt-4 btn btn-outline-primary-azul m-2 btn-lg btn-block rounded">
+          <button class="btn btn-outline-primary m-2 btn-lg btn-block rounded-pill">
             <Link to="/register" class="text-azul">Crear una cuenta</Link>
           </button>
         </div>
