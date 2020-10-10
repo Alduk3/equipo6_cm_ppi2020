b91455e23a538bb4abad01d21841c65b35ba400b
Jerson2001 <yerson200129@gmail.com>
2020-10-10 15:23:35 -0500

    Se agrega el avance de la pantalla de agregar un nuevo producto y un cambio mínimo en la pantalla de registro

--- frontend-test-project/src/components/App.js
+++ frontend-test-project/src/components/App.js
@@ -4,6 +4,7 @@ import Register from './Registro';
 import Inicio from './Inicio';
 import React from 'react';
 import Hogar from './Hogar'
+import NuevoProducto from './AgregarProducto'
 function App(){
 
     return(
@@ -13,6 +14,7 @@ function App(){
                 <Route path="/login"  component={Login}/>
                 <Route path="/register"  component={Register}/>
                 <Route path="/hogar" component={Hogar} />
+                <Route path="/nuevoproducto" component={NuevoProducto}/>
                 <Route component={Inicio} />
             </Switch>
         </Router>
