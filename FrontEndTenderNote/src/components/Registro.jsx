c064262bc2527a4d821ae6bd53ff80274d9ebbb4
Jerson2001 <yerson200129@gmail.com>
2020-10-15 16:37:00 -0500

    Pequeños cambios de rutas e interfaces del las pantallas registro y ajustes

--- FrontEndTenderNote/src/components/Registro.jsx
+++ FrontEndTenderNote/src/components/Registro.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import RegistroLogo from '../images/RegistroLogo.png'
+import User from '../images/User.png'
+import BotonM from '../images/BtnAgregar.png'
 import './styles/Registro.css'
 import { Link } from "react-router-dom";
 
@@ -10,17 +11,18 @@ function Registro() {
                 <div className="d-flex align-items-center">
                 <h1 className="p-3 align-self-center mx-auto letra-titulo text-justify text-center">Regístrate para continuar!</h1>
                 </div>
-                <div className="mx-auto d-flex justify-content-center mt-3">
-                    <img src={RegistroLogo} alt="" srcset="" class="img-fluid w-50"/>
-                </div>
+                <div className="img-perfil">
+              <span className="img"><img src={User} alt="" /></span>                
+              <a href="#"><img src={BotonM} type="icon" alt="" /></a>
+            </div>
             </div>
             <div class="col mx-auto">
                 <div class="col-md-10 d-flex flex-wrap align-middle p-4">
                     <input type="text" id="full_name" className="form-control btn-border mb-3 btn-lg font-weight-bold" placeholder="Nombre*" />
                     <input type="text" id="job" className="form-control btn-border mb-3 btn-lg font-weight-bold" placeholder="Correo*" />
                     <input type="text" id="celphone" className="form-control btn-border btn-lg font-weight-bold" placeholder="Contraseña*" />
-                    <p className="mt-2 font-weight-bold">*Todos los campos son obligatorios</p>
-                    <button className="btn btn-azul m-3 mt-5 btn-lg btn-block btn-border letra-titulo font-weight-bold">Regístrate</button>
+                    <p className="mt-2 font-weight-bold">*Todos los campos son obligatorios</p><Link to="/login">
+                   <button className="btn btn-azul m-3 mt-5 btn-lg btn-block btn-border letra-titulo font-weight-bold">Regístrate</button></Link>
                 </div>
             </div>
             <div className="text-center">
