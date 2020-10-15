1fdb302d75a4d7d6498979bf55cc85a5265f23a7
S-G-L <elsebasgallego2528@gmail.com>
2020-10-14 21:36:26 -0500

    se agregan los productos (provisional) en las pantallas de hogar e inventario

--- frontend-test-project/src/components/Hogar.js
+++ frontend-test-project/src/components/Hogar.js
@@ -4,6 +4,9 @@ import ajuste from '../images/ajustes.png'
 import hogar from '../images/hogar.png'
 import lista from '../images/lista.png'
 import usuario from '../images/usuario.png'
+import borrar1 from '../images/borrar1.png'
+import borrar2 from '../images/borrar2.png'
+import borrar3 from '../images/borrar3.png'
 import { Link } from 'react-router-dom'
 
 class Hogar extends React.Component {
@@ -22,6 +25,32 @@ class Hogar extends React.Component {
                         </Link>
                     </div>
                 </div>
+
+                <div className="col-fluid pt-5">
+                    <div className="mb-2">
+                        <h5 className="p-3 font-weight-bold">
+                            <u>Se están acabando</u>
+                        </h5>
+                        <div className="d-inline px-3 py-3">
+                            <img src={borrar1} className="border rounded shadow"/>
+                        </div>
+                        <div className="d-inline px-3 py-3">
+                            <img src={borrar2} className="border rounded shadow"/>
+                        </div>
+                    </div>
+                    <div className="mt-5">
+                        <h5 className="p-3 font-weight-bold">
+                            <u>Últimos productos agregados</u>
+                        </h5>
+                        <div className="d-inline px-3 py-4">
+                            <img src={borrar3} className="border rounded shadow"/>
+                        </div>
+                        <div className="d-inline px-3 py-4">
+                            <img src={borrar1} className="border rounded shadow"/>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="col-fluid fondo mx-auto row justify-content-center fixed-bottom">
                     <div className="d-inline-block p-2 px-4">
                         <Link to="/hogar">
--- frontend-test-project/src/components/Inventario.js
+++ frontend-test-project/src/components/Inventario.js
@@ -3,6 +3,9 @@ import './styles/Inventario.css'
 import ajuste from '../images/ajustes.png'
 import hogar from '../images/hogar.png'
 import lista from '../images/lista.png'
+import borrar1 from '../images/borrar1.png'
+import borrar2 from '../images/borrar2.png'
+import borrar3 from '../images/borrar3.png'
 import agregar from '../images/agregar.png'
 import { Link } from 'react-router-dom'
 
@@ -17,6 +20,30 @@ class Inventario extends React.Component {
                         </h1>
                     </div>
                 </div>
+                <div className="px-5 pt-2">
+                    <input type="text" id="full_name" className="form-control rounded-pill mb-3 btn-lg font-weight-bold" placeholder="Buscar" />
+                </div>
+                <br/>
+
+                <div className="text-center pt-2">
+                    <div className="d-inline-block px-3 py-3">
+                        <img src={borrar1} className="border rounded shadow"/>
+                    </div>
+                    <div className="d-inline-block px-3 py-3">
+                        <img src={borrar2} className="border rounded shadow"/>
+                    </div>
+                    <div className="d-inline-block px-3 py-4">
+                        <img src={borrar3} className="border rounded shadow"/>
+                    </div>
+                    <div className="d-inline-block px-3 py-4">
+                        <img src={borrar1} className="border rounded shadow"/>
+                    </div>
+                </div>
+                
+                <br/>
+                <br/>
+                <br/>
+                <br/>
                 <div className="float-right mb-4 mr-3 margen pb-5">
                     <Link>
                         <img src={agregar} className="img-fluid tamano-img-agregar" />
@@ -25,7 +52,7 @@ class Inventario extends React.Component {
                 <div className="col-fluid fondo mx-auto row justify-content-center fixed-bottom">
                     <div className="d-inline-block p-2 px-4">
                         <Link to="/hogar">
-                        <img src={hogar} className="img-fluid tamano-img mx-auto shadow" />
+                        <img src={hogar} className="img-fluid tamano-img mx-auto" />
                         </Link>
                     </div>
                     <div className="d-inline-block p-2 px-4 pl-5 pr-5">
