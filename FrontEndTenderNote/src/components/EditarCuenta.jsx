9a2d144596d450ba8eed30ee5a1055df19f58399
Juanpablobolivar <juanpablobolivarmarin0820@gmail.com>
2020-10-15 20:53:22 -0500

    correción rutas

--- FrontEndTenderNote/src/components/EditarCuenta.jsx
+++ FrontEndTenderNote/src/components/EditarCuenta.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import FlechaA from '../images/FlechaA.png'
 import Lapiz from '../images/Editar.png'
 import User from '../images/User.png'
-import Chulito from '../images/ChuloV.png'
 import { Link } from "react-router-dom";
 import './styles/EditarCuenta.css'
 
@@ -11,11 +10,11 @@ function EditarCuenta() {
         <div className='fond'>
           <title>Mi Cuenta</title>
           <div className="mx-auto d-flex fondo justify-content-center mb-3 d-flex justify-content-between">
-                <Link to="/inventario"><img src={FlechaA} alt="" srcset="" className="img-fluid pl-2" /></Link>
+                <Link to="/hogar"><img src={FlechaA} alt="" srcset="" className="img-fluid pl-2" /></Link>
                 <h3 className="text-blanco">
-                    Editar Cuenta
+                    Mi Cuenta
                         </h3>
-                <Link to="/inventario"><img src={Chulito} alt="" srcset="" className="img-fluid pr-2" /></Link>
+                <Link to="/cuenta"><img src={Lapiz} alt="" srcset="" className="Editar.png" /></Link>
             </div>
           <div className="container">
             <div className="img-perfil">
