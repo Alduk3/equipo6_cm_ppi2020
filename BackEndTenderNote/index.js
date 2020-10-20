70b9551d084d04762aa0446b0485e344285b4073
Jerson Álvarez Ramírez <yerson200129@gmail.com>
2020-10-20 21:47:31 +0000

    Se hicieron cambios en el backend y en el proyecto en general

--- BackEndTenderNote/index.js
+++ BackEndTenderNote/index.js
@@ -1,28 +1,24 @@
 const express = require("express");
+const path = require ("path")
+const morgan = require ("morgan")
 const app = express();
+
+//Middelwares
+app.use(morgan("dev"))
+app.use(express.json())
+
+//routes
+
+app.use("/api/", require("./routes/tendero.js"))
+
 
 app.get("/", (req, res) => {
   res.send("<h1>Hola, bienvenido a TenderNote</h1>");
 });
 
-app.get("/integrantes", (req, res) => {
-  res.send(`
-            <h2>Los integrantes son:</h2>
-            <p>Jerson Álvarez Ramirez</p>
-            <p>Juan Pablo Bolívar Marín</p>
-            <p>Alejandro Duque Florez</p>
-            <p>Sebastian Gallego Londoño</p>`);
+app.set("puerto", 8081)
+
+app.listen(app.get("puerto"), ()=>{
+  console.log("El servidor está activo en el puerto " + app.get("puerto"));
 });
 
-app.get("/descripcion", (req, res) => {
-  res.send(`<h1>TenderNote</h1>
-            <p>Aplicativo móvil para los tenderos independientes sobre el manejo y el control del inventario que estos llevan a sus locales. La aplicación cuenta con un registro de usuario (tendero) donde posteriormente esta información será guardada en una base de datos, al ingresar el usuario al aplicativo, podrá visualizar sus productos en una imagen con sus respectivos precios y cantidades y dará la opción de controlar su inventario sea porque se reciben productos de parte de su proveedor o se venden a sus clientes.</p>`);
-});
-app.listen(5057, function () {
-  console.log("El servidor está activo en el puerto 5057");
-});
-
-app.get("/sobre_nosotros", (req, res) => {
-  res.send(`<p><a href = "https://www.facebook.com/profile.php?id=100010175615706"> Jerson Álvarez Ramírez </a></p><p><a href = "https://www.facebook.com/juanpablo.bolivarmarin.7"> Juan Pablo Bolívar Marín</a></p>
-   <p><a href = "https://www.facebook.com/alejandro.duqueflorez" > Alejandro Duque Florez </a></p> <p><a href = "https://www.facebook.com/juan.londono.376043"> Sebastián Gallago Londoño</a></p>`);
-});
---
+++ BackEndTenderNote/routes/tendero.js
@@ -0,0 +1,75 @@
+const {Router} = require ("express")
+const router = Router
+const fs = require ("fs")
+//const FileTendero = fs.readSync("./tendero.json", "utf-8")
+//const JSONTendero = JSON.parse(FileTendero)
+
+//router.get("/", (req,res)=>{
+  //res.send("API REST TENDERO")
+//})
+
+//router.get("/tendero",(req,res)=>{
+//res.json(JSONTendero)
+//})
+
+//router.post("/preferenciadeportes", (req,res)=>{
+//let id = JSONTendero.length + 1
+//let {nombre, apellidos, correo} = req.body
+//let nuevoUsuario = {
+  //"id" : id,
+  //"nombre" : nombre,
+  //"apellidos" : apellidos,
+  //"correo" : correo
+//}
+//JSONTendero.push(nuevoUsuario)
+//fs.writeFileSync("./tendero.json", JSON.stringify(JSONTendero) , "utf-8")
+//res.status(201).json(nuevoUsuario)
+//})
+
+//router.get("/tendero/:id", (req,res) => {
+  //let id = req.params.id
+  //let tenderoEncontrado = JSONTendero.find
+  //(usuario => usuario.id == id)
+
+  //if(tenderoEncontrado != undefined)
+    //res.status(200).json(tenderoEncontrado)
+  //else
+    //res.json(`El ID ${id} no existe`)
+//})
+
+//router.put("/tendero/:id", (req,res) => {
+  //let id = req.params.id 
+  //let {nombre, apellidos, correo} = req.body
+
+  //let usuarioModificado = JSONTendero.find(usuario => {
+    //if(usuario.id == id){
+      //usuario.nombre = nombre
+      //usuario.apellidos = apellidos
+      //usuario.correo = correo
+      //return usuario
+    //}
+  //})
+
+  //if(usuarioModificado != undefined){
+    //fs.writeFileSync('./tendero.json', JSON.stringify(JSONTendero), 'utf-8')
+    //res.status(201).json(usuarioModificado)
+  //}else{
+    //res.status(200).json(`El ID ${id} no existe`)
+  //}
+//})
+
+//router.delete("/tendero/:id", (req, res) => {
+  //let id = req.params.id
+  //let indexUsuario = JSONTendero.findIndex
+  //(usuario => usuario.id == id)
+  //if(indexUsuario != -1){
+    //JSONTendero.splice(indexUsuario, 1)
+    //fs.writeFileSync('./tendero.json', JSON.stringify(JSONTendero), 'utf-8')
+    //res.status(200).json({mensaje : `Usuario ${id} fue eliminado`})
+  //}else{
+    //res.json(`El id ${id} no existe`)
+  //}
+//})
+
+
+module.exports = router
