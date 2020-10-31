bfd582f986db3549bc0cfca18bf3f1cbede1757d
Jerson2001 <yerson200129@gmail.com>
2020-10-31 16:19:22 -0500

    Cambios en el backend y instalación de las dependecias del MySQL

--- BackEndTenderNote/routes/tendero.js
+++ BackEndTenderNote/routes/tendero.js
@@ -1,50 +1,65 @@
 const { Router } = require("express");
 const router = Router();
 const fs = require("fs");
+var app = require('express')()
 
-const FileTendero = fs.readFileSync("./tendero.json", "utf-8");
-const JSONTendero = JSON.parse(FileTendero);
+let FileTendero = null
+let JSONTendero = null
+
+const loadFile = (req) => {
+  const FileTendero = fs.readFileSync(`${req.app.get('ABSOLUTE_PATH')}tendero.json`, 'utf-8');
+  const JSONTendero = JSON.parse(FileTendero);
+}
+
 
 router.get("/", (req, res) => {
   res.send("API REST PREFERENCIA TENDERO");
 });
 
 router.get("/tendero", (req, res) => {
+  loadFile(req)
   res.json(JSONTendero);
 });
 
-router.post("/tendero", (req,res)=>{
-let id = JSONTendero.length + 1
-let {nombre, apellidos, correo} = req.body
-let nuevoUsuario = {
-  "id": id,
-  "nombre" : nombre,
-  "apellidos" : apellidos,
-  "correo" : correo
-}
+router.post("/tendero", (req, res) => {
+  let id = JSONTendero.length + 1
+  loadFile(req)
+  let {
+    nombre,
+    apellidos,
+    correo
+  } = req.body
 
-JSONTendero.push(nuevoUsuario)
-fs.writeFileSync("./tendero.json", JSON.stringify(JSONTendero) , "utf-8")
-res.status(201).json(nuevoUsuario)
+  let nuevoUsuario = {
+    "id": id,
+    "nombre": nombre,
+    "apellidos": apellidos,
+    "correo": correo
+  }
+
+  JSONTendero.push(nuevoUsuario)
+  fs.writeFileSync("./tendero.json", JSON.stringify(JSONTendero), "utf-8")
+  res.status(200).json(nuevoUsuario)
 })
 
-router.get("/tendero/:id", (req,res) => {
+router.get("/tendero/:id", (req, res) => {
   let id = req.params.id
+  loadFile(req)
   let tenderoEncontrado = JSONTendero.find
-  (usuario => usuario.id == id)
+    (usuario => usuario.id == id)
 
-  if(tenderoEncontrado != undefined)
+  if (tenderoEncontrado != undefined)
     res.status(200).json(tenderoEncontrado)
   else
-    res.json(`El ID ${id} no existe`)
+    res.status(200).json({})
 })
 
-router.put("/tendero/:id", (req,res) => {
-  let id = req.params.id 
-  let {nombre, apellidos, correo} = req.body
-
+router.put("/tendero/:id", (req, res) => {
+  let id = req.params.id
+  let { nombre, apellidos, correo } = req.body
+  loadFile(req)
   let usuarioModificado = JSONTendero.find(usuario => {
-    if(usuario.id == id){
+    if (usuario.id == id) {
       usuario.nombre = nombre
       usuario.apellidos = apellidos
       usuario.correo = correo
@@ -52,24 +67,25 @@ router.put("/tendero/:id", (req,res) => {
     }
   })
 
-  if(usuarioModificado != undefined){
+  if (usuarioModificado != undefined) {
     fs.writeFileSync('./tendero.json', JSON.stringify(JSONTendero), 'utf-8')
-    res.status(201).json(usuarioModificado)
-  }else{
-    res.status(200).json(`El ID ${id} no existe`)
+    res.status(200).json(usuarioModificado)
+  } else {
+    res.status(200).json(`El tendero no existe`)
   }
 })
 
 router.delete("/tendero/:id", (req, res) => {
   let id = req.params.id
+  loadFile(req)
   let indexUsuario = JSONTendero.findIndex
-  (usuario => usuario.id == id)
-  if(indexUsuario != -1){
+    (usuario => usuario.id == id)
+  if (indexUsuario != -1) {
     JSONTendero.splice(indexUsuario, 1)
     fs.writeFileSync('./tendero.json', JSON.stringify(JSONTendero), 'utf-8')
-    res.status(200).json({mensaje : `Usuario ${id} fue eliminado`})
-  }else{
-    res.json(`El id ${id} no existe`)
+    res.status(200).json(indexUsuario + 1)
+  } else {
+    res.status(200).json(`El tendero no existe`)
   }
 })
 
