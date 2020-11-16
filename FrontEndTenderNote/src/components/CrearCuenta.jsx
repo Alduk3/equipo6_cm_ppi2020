a8eb096790c53e11402d51afbff8599f7ca17bd8
S-G-L <elsebasgallego2528@gmail.com>
2020-11-16 18:09:54 -0500

    se agrega el boton de Google

--- FrontEndTenderNote/src/components/CrearCuenta.jsx
+++ FrontEndTenderNote/src/components/CrearCuenta.jsx
@@ -3,7 +3,14 @@ import './styles/CrearCuenta.css'
 import googleIcon from '../images/google-icon.svg'
 import { Link } from "react-router-dom";
 
+import { GoogleLogin } from 'react-google-login';
+
 function CrearCuenta() {
+
+    const responseGoogle = (response) => {
+        console.log(response);
+      }
+
     return (
         <div className="col-fluid login-container row-fluid">
             <div className="row-auto">
@@ -13,9 +20,18 @@ function CrearCuenta() {
             </div>
             <div class="col mx-auto mt-4">
             <div className="col-fluid registro-boton mx-auto">
-                    <Link to="#">
+
+                <GoogleLogin 
+                    className="btn btn-blanco  mt-3 btn-lg btn-block google-login letra-titulo2 font-weight-bold"
+                    clientId="482073054770-9f7thkhmrca1k7rl8cquroceriooclm3.apps.googleusercontent.com"
+                    buttonText="Regístrate con Google"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
+                    {/* <Link to="#">
                         <button className="btn btn-blanco  mt-3 btn-lg btn-block btn-border letra-titulo2 font-weight-bold"><a href="#"><img src={googleIcon} type="icon" alt="" width="23" className="mr-2 "/></a>Regístrate con google</button>
-                    </Link>
+                    </Link> */}
                 </div>
                 <br/>
                 <div className="col-fluid registro-boton mx-auto">
