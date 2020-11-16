9af945b193777de1846b598715741e1f32a4528b
Jerson Álvarez Ramírez <yerson200129@gmail.com>
2020-11-16 22:25:24 +0000

    Métodos de la tabla de producto

--- BackEndTenderNote/routes/notificaciones.js
+++ BackEndTenderNote/routes/notificaciones.js
@@ -1,1 +1,15 @@
-import {Router } = require ('express)
+const {Router } = require ('express');
+const router = Router ();
+const connection = require ('./../db/mysql');
+
+router.get ('/notificaciones', (req,res) => {
+  try{ connection.query('SELECT * FROM `Notificaciones`', (error, rows, fields) =>{
+    if (!error) {
+      res.status (503).json ({mensaje:"Ha ocurrido un error en el servidor"})
+    }
+  })
+
+  } catch (error) {
+    res.json (rows)
+  }
+})
--- BackEndTenderNote/routes/producto.js
+++ BackEndTenderNote/routes/producto.js
@@ -1,4 +1,4 @@
-const { Router } = require("express");
+/*const { Router } = require("express");
 const router = Router();
 const fs = require("fs");
 var app = require('express')()
@@ -93,4 +93,116 @@ router.get("/", (req, res) => {
     }
   })
 
-module.exports = router;
+module.exports = router;*/
+
+const {Router} = require('express')
+const router = Router()
+const {connection} = require('./../db/mysql')
+//connection.connect()
+
+router.get('/producto', (req, res) => {
+  try{
+    connection.query("SELECT * FROM producto", (error, rows, fields) => {
+      if(error){
+        res.status(503).json({mensaje : "Error en el servidor.", error : true, errorDB : error})
+      }
+      res.json(rows)
+    })
+  }catch(error){
+    res.status(503).json({mensaje : "Error en el servidor.", error : true})
+  }
+})
+
+router.get('/producto/:id_producto', (req, res) => {
+  try{
+    const id = req.params.id
+    connection.query(`SELECT * 
+                      FROM producto
+                      WHERE producto = ?`, [id])
+  }catch(error){
+    res.status(503).json({mensaje : "Error en el servidor.", error : true})
+  }
+})
+
+router.put('/producto/:id_producto', (req, res) => {
+  try{
+    const id_producto = req.params.id
+    const {
+      nombre,
+      cantidad,
+      valor,
+      id_usuario
+    } = req.body
+
+    connection.query(`UPDATE producto
+                      SET nombre = ?, cantidad = ?, valor = ?, id_usuario = ? WHERE id_producto = ?`,[nombre, cantidad, valor,  id_usuario, id_producto], (error, resulset, fields) => {
+                        if(error){
+                          res.status(502).json({mensaje: "Error en motor de base de datos."})
+                        }else{
+                          res.status(201).json({
+                            id_producto : id_producto,
+                            nombre : nombre,
+                            cantidad : cantidad,
+                            valor : valor,
+                            id_usuario : id_usuario
+                          })
+                        }
+                      } 
+                    )
+
+ //   console.log(id)
+  }catch(error){
+    res.status(502).json({mensaje : "Error en el servidor."})
+  }
+})
+
+router.post('/producto', (req, res) => {
+  try{
+    const {
+      nombre,
+      cantidad,
+      valor,
+      id_usuario
+    } = req.body    
+    const SQL = `INSERT INTO producto (nombre, cantidad, valor, id_usuario) 
+                       VALUES(?,?,?,?)`
+    const parametros = [nombre, cantidad, valor, id_usuario]
+    connection.query(SQL, parametros, (error, results, fields) => {
+      if(error){
+        console.log(error)
+        res.status(502).json({mensaje : 'Error ejecutando la consulta.'})
+      }else{
+        console.log(results)
+        res.status(201).json({
+                              id_producto : results.insertId,
+                              nombre: nombre, 
+                              cantidad: cantidad,
+                              valor: valor,
+                              id_usuario: id_usuario})
+      }
+    })
+  }catch(error){
+    res.status(502).json({mensaje:"Error en el servidor"})
+  }
+})
+
+router.delete('/producto/:id', (req, res) => {
+  try{
+    const {id} = req.params
+    const SQL = `DELETE FROM producto WHERE id_producto = ?`
+    connection.query(SQL, [id], (error, results, fields) => {
+      if(error){
+        res.status(502).json({mensaje : 'Error ejecutando la consulta'})
+      }else{
+        if(results.affectedRows > 0)
+          res.json({mensaje : "Registro eliminado"})
+        else
+          res.json({mensaje : "El registro no existe"})
+      }
+    })
+  }catch(error){
+    res.status(502).json({mensaje:"Error en el servidor"})
+  }
+})
+  
+module.exports = router
