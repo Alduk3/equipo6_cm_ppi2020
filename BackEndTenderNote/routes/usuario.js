c7cfbed8c6dab268d93504cb2757676089a96ffb
Jerson Álvarez Ramírez <yerson200129@gmail.com>
2020-11-16 19:41:09 +0000

    Métodos de la tabla usuario y varios cambios

--- BackEndTenderNote/routes/usuario.js
+++ BackEndTenderNote/routes/usuario.js
@@ -1,113 +1,108 @@
-//const { Router } = require("express");
-//const router = Router();
-//const fs = require("fs");
-//var app = require('express')()
-//const FileTendero = fs.readFileSync("./usuario.json", "utf-8")
-//const JSONTendero = JSON.parse(FileTendero)
-
-//let FileTendero = null
-//let JSONTendero = null
-
-//const loadFile = (req) => {
-  //const FileTendero = fs.readFileSync(`${req.app.get('ABSOLUTE_PATH')}usuario.json`, 'utf-8');
-  //const JSONTendero = JSON.parse(FileTendero);
-//}
-
-
-//router.get("/", (req, res) => {
-  //res.send("API REST USUARIO");
-//});
-
-//router.get("/usuario", (req, res) => {
-  //loadFile(req)
-  //res.json(JSONTendero);
-//});
-
-//router.post("/usuario", (req, res) => {
-  //let id = JSONTendero.length + 1 
-  //loadFile(req)
-  //let {
-    //nombre,
-    //apellidos,
-    //correo
-  //} = req.body
-
-  //let nuevoUsuario = {
-    //"id_usuario": id,
-    //"nombre": nombre,
-    //"apellidos": apellidos,
-    //"correo": correo
-  //}
-
-  //JSONTendero.push(nuevoUsuario)
-  //fs.writeFileSync("./usuario.json", JSON.stringify(JSONTendero), "utf-8")
-  //res.status(200).json(nuevoUsuario)
-//})
-
-//router.get("/usuario/:id", (req, res) => {
-  //let id = req.params.id
-  //loadFile(req)
-  //let tenderoEncontrado = JSONTendero.find
-    //(usuario => usuario.id == id)
-
-  //if (tenderoEncontrado != undefined)
-    //res.status(200).json(tenderoEncontrado)
-  //else
-    //res.status(200).json({})
-//})
-
-//router.put("/usuario/:id", (req, res) => {
-  //let id = req.params.id
-  //let { nombre, apellidos, correo } = req.body
-  //loadFile(req)
-  //let usuarioModificado = JSONTendero.find(usuario => {
-    //if (usuario.id == id) {
-      //usuario.nombre = nombre
-      //usuario.apellidos = apellidos
-      //usuario.correo = correo
-      //return usuario
-    //}
-  //})
-
-  //if (usuarioModificado != undefined) {
-    //fs.writeFileSync('./usuario.json', JSON.stringify(JSONTendero), 'utf-8')
-    //res.status(200).json(usuarioModificado)
-  //} else {
-    //res.status(200).json(`El tendero no existe`)
-  //}
-//})
-
-//router.delete("/usuario/:id", (req, res) => {
-  //let id = req.params.id
-  //loadFile(req)
-  //let indiceUsuario = JSONTendero.findIndex
-    //(usuario => usuario.id == id)
-  //if (indiceUsuario != -1) {
-    //JSONTendero.splice(indiceUsuario, 1)
-    //fs.writeFileSync('./usuario.json', JSON.stringify(JSONTendero), 'utf-8')
-    //res.status(200).json(indiceUsuario + 1)
-  //} else {
-    //res.status(200).json(`El tendero no existe`)
-  //}
-//})
-
-//module.exports = router;
-
-const { Router } = require("express")
+const {Router} = require('express')
 const router = Router()
-const {connection} = require('../db/mysql')
-    
-router.get("/usuario", (req, res) => {
-    //connection.connect()
-    connection.query('SELECT * FROM usuario',  (error, rows, fields) => {
-        if(!error){
-          connection.end()
-          res.json(rows)
-        }else{
-            res.json({error: "Error ejecutando la consulta"})
-        }
+const {connection} = require('./../db/mysql')
+//connection.connect()
+
+router.get('/usuario', (req, res) => {
+  try{
+    connection.query("SELECT * FROM usuario", (error, rows, fields) => {
+      if(error){
+        res.status(503).json({mensaje : "Error en el servidor.", error : true, errorDB : error})
+      }
+      res.json(rows)
     })
+  }catch(error){
+    res.status(503).json({mensaje : "Error en el servidor.", error : true})
+  }
 })
 
+router.get('/usuario/:id_usuario', (req, res) => {
+  try{
+    const id = req.params.id
+    connection.query(`SELECT * 
+                      FROM usuario
+                      WHERE id_usuario = ?`, [id])
+  }catch(error){
+    res.status(503).json({mensaje : "Error en el servidor.", error : true})
+  }
+})
+
+router.put('/usuario/:id_usuario', (req, res) => {
+  try{
+    const id_usuario = req.params.id
+    const {
+      nombre,
+      apellido,
+      correo
+    } = req.body
+
+    connection.query(`UPDATE usuario
+                      SET nombre = ?, apellido = ?,
+                      correo = ? WHERE id_usuario = ?`,[nombre, apellido, correo, id_usuario], (error, resulset, fields) => {
+                        if(error){
+                          res.status(502).json({mensaje: "Error en motor de base de datos."})
+                        }else{
+                          res.status(201).json({
+                            id_usuario : id_usuario,
+                            nombre : nombre,
+                            apellido : apellido,
+                            correo : correo
+                          })
+                        }
+                      } 
+                    )
+
+ //   console.log(id)
+  }catch(error){
+    res.status(502).json({mensaje : "Error en el servidor."})
+  }
+})
+
+router.post('/usuario', (req, res) => {
+  try{
+    const {
+      nombre,
+      apellido,
+      correo
+    } = req.body    
+    const SQL = `INSERT INTO usuario (nombre, apellido,correo) 
+                       VALUES(?,?,?)`
+    const parametros = [nombre, apellido, correo]
+    connection.query(SQL, parametros, (error, results, fields) => {
+      if(error){
+        console.log(error)
+        res.status(502).json({mensaje : 'Error ejecutando la consulta.'})
+      }else{
+        console.log(results)
+        res.status(201).json({
+                              id_usuario : results.insertId,
+                              nombre : nombre,
+                              apellido : apellido,
+                              correo : correo})
+      }
+    })
+  }catch(error){
+    res.status(502).json({mensaje:"Error en el servidor"})
+  }
+})
+
+router.delete('/usuario/:id', (req, res) => {
+  try{
+    const {id} = req.params
+    const SQL = `DELETE FROM usuario WHERE id_usuario = ?`
+    connection.query(SQL, [id], (error, results, fields) => {
+      if(error){
+        res.status(502).json({mensaje : 'Error ejecutando la consulta'})
+      }else{
+        if(results.affectedRows > 0)
+          res.json({mensaje : "Registro eliminado"})
+        else
+          res.json({mensaje : "El registro no existe"})
+      }
+    })
+  }catch(error){
+    res.status(502).json({mensaje:"Error en el servidor"})
+  }
+})
   
 module.exports = router
