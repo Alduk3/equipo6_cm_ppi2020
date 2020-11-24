84446420a3f85692983275ca234bd060cdd0bc51
Jerson2001 <yerson200129@gmail.com>
2020-11-24 17:23:47 -0500

    Correción de variables de entorno

--- BackEndTenderNote/index.js
+++ BackEndTenderNote/index.js
@@ -24,8 +24,8 @@ app.get("/",(req,res)=>{
 res.send("API EQUIPO 6")
 })
 
-app.set("ABSOLUTE_PATH", `${__dirname}/`)
-app.set("port", process.env.Port || 5000)
+//app.set("ABSOLUTE_PATH", `${__dirname}/`)
+app.set("port", process.env.PORT || 8080)
 
 app.listen(app.get("port"),()=>{
 console.log("El servidor está corriendo en el puerto " + app.get("port"))
